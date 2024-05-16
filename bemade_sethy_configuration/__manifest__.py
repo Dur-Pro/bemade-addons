9d80e83d1ad30448f9ddfb7bc9e86cacd5e00bfe
Marc Durepos <marc@bemade.org>
2024-05-15 21:01:40 -0400

    Update GPL to AGPL to respect partner_multi_relation license.

--- bemade_sethy_configuration/__manifest__.py
+++ bemade_sethy_configuration/__manifest__.py
@@ -8,13 +8,12 @@
     'author': 'Bemade',
     'website': 'https://bemade.org',
     'email': 'it@bemade.org',
-    'license': 'GPL-3',
+    'license': 'AGPL-3',
     'depends': [
         # List of module dependencies
         'base',
         'contacts',
         'crm',
-        'document_knowledge',
         'document_page',
         'document_page_approval',
         'attachment_indexation',
